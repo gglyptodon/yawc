c8e6917f52a71ca1fc0f33e23cb277de804ce608
Janina Mass <janina.mass@gmail.com>
2022-08-16 18:34:58 +0200

    basics running

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,29 @@ mod wasm_bindings;
 
 use crate::lib::Game;
 fn main() {
-    println!("Hello, world!");
+    let mut g = Game::new();
+    g.attempt(String::from("TESLA")).unwrap();
+    print!("\n--{}--\n", g.attempts_remaining);
+    for l in &g.letters {
+        print!("{}", l.1);
+    }
+    g.attempt(String::from("AISLE")).unwrap();
+    print!("\n--{}--\n", g.attempts_remaining);
+    for l in &g.letters {
+        print!("{}", l.1);
+    }
+    g.attempt(String::from("IDIOT")).unwrap();
+    print!("\n--{}--\n", g.attempts_remaining);
+
+    for l in &g.letters {
+        print!("{}", l.1);
+    }
+
+    print!("\n---\n");
+    g.attempt(String::from(&g.target)).unwrap();
+    for l in &g.letters {
+        print!("{}", l.1)
+    }
+    print!("\n--{}--\n", g.attempts_remaining);
+    println!("\n{:^10}\n", g.target);
 }
