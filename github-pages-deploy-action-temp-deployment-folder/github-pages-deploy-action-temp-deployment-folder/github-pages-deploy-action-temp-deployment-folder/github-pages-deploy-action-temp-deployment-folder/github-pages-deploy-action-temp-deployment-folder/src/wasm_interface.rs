4552bb38080d315b3046675a93869c5286bd2dcc
gglyptodon <janina.mass@gmail.com>
2022-08-18 11:12:32 +0000

    Deploying to gh-pages from @ gglyptodon/yawc@7623c599d1219b29405fe8a370486c06e0591e3c 🚀

---
+++ github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/src/wasm_interface.rs
@@ -0,0 +1,64 @@
+use crate::yawc::*;
+use std::cell::RefCell;
+use wasm_bindgen::prelude::*;
+
+thread_local! {
+   static YAWC: RefCell<Game> = RefCell::new(Game::new());
+}
+
+#[wasm_bindgen(js_name=newGame)]
+pub fn new_game() {
+    YAWC.with(|y| y.borrow_mut().reconfigure())
+}
+
+#[wasm_bindgen(js_name=getState)]
+pub fn get_state() -> String {
+    YAWC.with(|y| y.borrow().to_string())
+}
+
+#[wasm_bindgen(js_name=showAttempts)]
+pub fn show_attempts() -> String {
+    YAWC.with(|y| y.borrow().show_attemps())
+}
+#[wasm_bindgen(js_name=showAttemptsRes)]
+pub fn show_attempts_res() -> String {
+    YAWC.with(|y| y.borrow().show_attemps_res())
+}
+#[wasm_bindgen(js_name=getAttemptsRemaining)]
+pub fn get_attempts_remaining() -> u8 {
+    YAWC.with(|y| y.borrow().attempts_remaining)
+}
+
+#[wasm_bindgen(js_name=getWord)]
+pub fn get_word() -> String {
+    YAWC.with(|y| y.borrow().target.clone())
+}
+
+#[wasm_bindgen(js_name=attemptWord)]
+pub fn attempt_word(word: String) {
+    YAWC.with(|y| {
+        let _ = y.borrow_mut().attempt(word);
+    })
+}
+
+#[wasm_bindgen(js_name=getKbUsed)]
+pub fn get_kb_used(character: char) -> bool {
+    YAWC.with(|y| {
+        y.borrow().is_used(character) //todo this is not a nice workaround
+    })
+}
+
+#[wasm_bindgen(js_name=getKbIncorrect)]
+pub fn get_kb_incorrect(character: char) -> bool {
+    YAWC.with(|y| y.borrow().is_at_incorrect_position(character))
+}
+
+#[wasm_bindgen(js_name=getKbCorrect)]
+pub fn get_kb_correct(character: char) -> bool {
+    YAWC.with(|y| y.borrow().is_at_correct_position(character))
+}
+
+#[wasm_bindgen(js_name=isWon)]
+pub fn is_won() -> bool {
+    YAWC.with(|y| y.borrow().is_won)
+}
