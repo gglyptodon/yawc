f719fb9aac3527a3cf0de896ba74b53d4105f12a
gglyptodon <janina.mass@gmail.com>
2022-08-19 09:58:48 +0000

    Deploying to gh-pages from @ gglyptodon/yawc@b1d31b1684c85db576aefba63971f9c01a513438 🚀

---
+++ github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/misc/main.rs
@@ -0,0 +1,28 @@
+use crate::yawc::Game;
+fn main() {
+    let mut g = Game::new();
+    g.attempt(String::from("TESLA")).unwrap();
+    print!("\n--{}--\n", g.attempts_remaining);
+    for l in &g.letters {
+        print!("{}", l.1);
+    }
+    g.attempt(String::from("AISLE")).unwrap();
+    print!("\n--{}--\n", g.attempts_remaining);
+    for l in &g.letters {
+        print!("{}", l.1);
+    }
+    g.attempt(String::from("IDIOT")).unwrap();
+    print!("\n--{}--\n", g.attempts_remaining);
+
+    for l in &g.letters {
+        print!("{}", l.1);
+    }
+
+    print!("\n---\n");
+    g.attempt(String::from(&g.target)).unwrap();
+    for l in &g.letters {
+        print!("{}", l.1)
+    }
+    print!("\n--{}--\n", g.attempts_remaining);
+    println!("\n{:^10}\n", g.target);
+}
